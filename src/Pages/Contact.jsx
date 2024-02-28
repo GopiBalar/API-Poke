f025153770db2f4011208c63eaf36b24568cb4e5
GopiBalar <gopibalar1234@gmail.com>
2024-02-28 15:15:21 +0530

    useSearchParams

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { useOutletContext } from "react-router-dom";
+import { useOutletContext, useSearchParams } from "react-router-dom";
 
 function Contact() {
   const ctx = useOutletContext();
-  
-  return <div>Contact {ctx}</div>;
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams1, setSearchParams1] = useSearchParams();
+
+  const city = searchParams.get("city");
+  const age = searchParams1.get("age");
+
+  return (
+    <>
+      <div>Contact {ctx}</div>
+      <h3>{city}</h3>
+      <h3>{age}</h3>
+      <input
+        type="text"
+        onChange={(e) => setSearchParams({ city: e.target.value, age: 40 })}
+      />
+      {/* <button onClick={(e) => setSearchParams1({ age: 40 })}>set Age</button> */}
+    </>
+  );
 }
 
 export default Contact;
